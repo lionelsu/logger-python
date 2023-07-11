c6388d4040b8afff2df6a990a4f2c630932da3e8
lionelsu <lionel_suicune@hotmail.com>
2023-07-11 09:51:41 -0400

    feat: used unicode to generate colors

--- logger.py
+++ logger.py
@@ -1,33 +1,37 @@
 from time import strftime
 
 
-class Log:
-    CLR_WARNING: str = '\033[93m'
-    CLR_ERR: str = '\033[92m'
-    CLR_END: str = '\033[0m'
-    CLR_MSG: str = '\033[94m'
-    CLR_SUCCESS: str = '\033[92m'
+class Logger:
+    CLR_WARNING: str = '\u001b[93m'
+    CLR_ERR: str = '\u001b[91m'
+    CLR_END: str = '\u001b[0m'
+    CLR_MSG: str = '\u001b[94m'
+    CLR_SUCCESS: str = '\u001b[92m'
 
     @staticmethod
     def _format(msg: str) -> str:
         return f'[{strftime("%Y-%m-%d %H:%M:%S")}] {msg}'
 
     @classmethod
+    def _log(cls, msg: str, color: str) -> None:
+        formatted_msg = f'{color}{cls._format(msg)}{cls.CLR_END}'
+        print(formatted_msg, flush=True)
+
+    @classmethod
     def msg(cls, msg: str) -> None:
-        print(f'{cls.CLR_MSG}{cls._format(msg)}{cls.CLR_END}', flush=True)
-
-    @classmethod
-    def warning(cls, msg: str) -> None:
-        print(f'{cls.CLR_WARNING}{cls._format(msg)}{cls.CLR_END}', flush=True)
-
-    @classmethod
-    def err(cls, msg: str) -> None:
-        print(f'{cls.CLR_ERR}{cls._format(msg)}{cls.CLR_END}', flush=True)
+        cls._log(msg, cls.CLR_MSG)
 
     @classmethod
     def success(cls, msg: str) -> None:
-        print(f'{cls.CLR_SUCCESS}{cls._format(msg)}{cls.CLR_END}', flush=True)
+        cls._log(msg, cls.CLR_SUCCESS)
 
     @classmethod
-    def debug(cls, msg: str) -> None:
-        print(cls._format(msg), flush=True)
+    def warn(cls, msg: str) -> None:
+        cls._log(msg, cls.CLR_WARNING)
+
+    @classmethod
+    def error(cls, msg: str) -> None:
+        cls._log(msg, cls.CLR_ERR)
+
+
+log = Logger()
