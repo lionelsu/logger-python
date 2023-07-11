53c9c9168a0b3a46ae3f47ff97fd9908c52fbd5f
lionelsu <lionel_suicune@hotmail.com>
2023-07-11 10:13:09 -0400

    refactor: moved hardcoded variables to constant like. removed the unecessary use of cls.CLR... in classmethods

--- logger.py
+++ logger.py
@@ -1,37 +1,38 @@
 from time import strftime
 
+UNI_ESC: str = '\u001b'
+CLR_WARNING: str = '%s[93m' % UNI_ESC
+CLR_ERR: str = '%s[91m' % UNI_ESC
+CLR_END: str = '%s[0m' % UNI_ESC
+CLR_MSG: str = '%s[94m' % UNI_ESC
+CLR_SUCCESS: str = '%s[92m' % UNI_ESC
+
 
 class Logger:
-    CLR_WARNING: str = '\u001b[93m'
-    CLR_ERR: str = '\u001b[91m'
-    CLR_END: str = '\u001b[0m'
-    CLR_MSG: str = '\u001b[94m'
-    CLR_SUCCESS: str = '\u001b[92m'
-
     @staticmethod
     def _format(msg: str) -> str:
         return f'[{strftime("%Y-%m-%d %H:%M:%S")}] {msg}'
 
     @classmethod
     def _log(cls, msg: str, color: str) -> None:
-        formatted_msg = f'{color}{cls._format(msg)}{cls.CLR_END}'
+        formatted_msg = f'{color}{cls._format(msg)}{CLR_END}'
         print(formatted_msg, flush=True)
 
     @classmethod
     def msg(cls, msg: str) -> None:
-        cls._log(msg, cls.CLR_MSG)
+        cls._log(msg, CLR_MSG)
 
     @classmethod
     def success(cls, msg: str) -> None:
-        cls._log(msg, cls.CLR_SUCCESS)
+        cls._log(msg, CLR_SUCCESS)
 
     @classmethod
     def warn(cls, msg: str) -> None:
-        cls._log(msg, cls.CLR_WARNING)
+        cls._log(msg, CLR_WARNING)
 
     @classmethod
     def error(cls, msg: str) -> None:
-        cls._log(msg, cls.CLR_ERR)
+        cls._log(msg, CLR_ERR)
 
 
 log = Logger()
